78049b245af29f9e9626848edc26e083bc3b794e
Ryan <ryantmeyer98@gmail.com>
2024-02-08 12:08:28 -0600

    Breaking for lunch, spent the morning comparing the average method i wrote and bills more exact method for caluclating the percent reamining. Bill's method is much better, will spend this afternoon cleaning and organizing code, but want to save before lunch.

--- scripts/02 percent remaining calculations.R
+++ scripts/02 percent remaining calculations.R
@@ -25,6 +25,8 @@ decomp.df <- decomp.df %>%
 decomp.df <- decomp.df %>%
   filter(!is.na(sample_time))
 
+bill.df <- decomp.df
+
 decomp.df <- decomp.df %>%
   mutate(forage_pct_remain = (forage_final_drywt_g / forage_initial_drywt_g) * 100) %>%
   mutate(tea_pct_remain = (tea_final_drywt_g / tea_initial_drywt_g) * 100)
@@ -57,7 +59,6 @@ tea_initial_prop_c <- decomp.df %>%
   filter(sample_time == "t0") %>%
   group_by(crop) %>%
   summarize(tea_initial_prop_c = mean(tea_prop_c, na.rm = TRUE))
-
 # AR: 0.4902500; CR: 0.4682500; GPC: 0.4793333; PCRO: 0.4218333; WPC: 0.4680000
 
 # tea nitrogen
@@ -65,7 +66,6 @@ tea_initial_prop_n <- decomp.df %>%
   filter(sample_time == "t0") %>%
   group_by(crop) %>%
   summarize(tea_initial_prop_n = mean(tea_prop_n, na.rm = TRUE))
-
 # AR: 0.04180000; CR: 0.03485000; GPC:0.04123333; PCRO: 0.03406667 WPC: 0.03970000
 
 # multiply each sample by the initial proportion of carbon to get the initial for every sample
@@ -116,17 +116,78 @@ decomp.df <- decomp.df %>%
          tea_final_carbon_g = tea_final_drywt_g * tea_prop_c,
          tea_final_nitrogen_g = tea_final_drywt_g * tea_prop_n)
 
+# calculate the percent remaining
 decomp.df <- decomp.df %>%
-  mutate(forage_pct_c_remaining = (forage_final_carbon_g / forage_initial_carbon_g) * 100) %>%
-  mutate(forage_pct_n_remaining = (forage_final_nitrogen_g / forage_initial_nitrogen_g) * 100) %>%
-  mutate(tea_pct_n_remaining = (tea_final_carbon_g / tea_initial_carbon_g) * 100) %>%
-  mutate(tea_pct_c_remaining = (tea_final_nitrogen_g / tea_initial_nitrogen_g) * 100)
+  mutate(forage_pct_c_remain = (forage_final_carbon_g / forage_initial_carbon_g) * 100) %>%
+  mutate(forage_pct_n_remain = (forage_final_nitrogen_g / forage_initial_nitrogen_g) * 100) %>%
+  mutate(tea_pct_n_remain = (tea_final_carbon_g / tea_initial_carbon_g) * 100) %>%
+  mutate(tea_pct_c_remain = (tea_final_nitrogen_g / tea_initial_nitrogen_g) * 100)
 
+# BILL'S METHOD ----
+# forage bags ----
+bill.df <- decomp.df %>%
+  mutate(
+    forage_mass_loss = forage_initial_drywt_g - forage_final_drywt_g,
+    forage_pct_remain = (forage_final_drywt_g / forage_initial_drywt_g) * 100) %>%
+# calculate proportion of nutrient remaining
+  mutate(
+    forage_prop_c = forage_pct_c / 100, 
+    forage_prop_n = forage_pct_n / 100) %>%
+# calculate final weight of nutrient
+  mutate(
+    forage_final_c_g = forage_prop_c * forage_final_drywt_g,
+    forage_final_n_g = forage_prop_n * forage_final_drywt_g) %>%
+# calculate initial weight of nutrient
+  mutate(
+    forage_initial_c_g = forage_prop_c * forage_initial_drywt_g,
+    forage_initial_n_g = forage_prop_n * forage_initial_drywt_g) %>%
+# calculate percent remainig 
+  mutate(forage_pct_c_remain = (forage_final_c_g / forage_initial_c_g) * 100,
+         forage_pct_n_remain = (forage_final_n_g / forage_initial_n_g) * 100)
+
+# tea bags ----
+bill.df <- bill.df %>%
+  # tea percent remaining
+  mutate(
+    tea_mass_loss = tea_initial_drywt_g - tea_final_drywt_g,
+    tea_pct_remain = (tea_final_drywt_g / tea_initial_drywt_g) * 100) %>%
+  # turning the percent into a proportion 
+  mutate(
+    tea_prop_c = tea_pct_c / 100, 
+    tea_prop_n = tea_pct_n / 100) %>%
+  # calculate final weight of nutrient per sample
+  mutate(
+    tea_final_c_g = tea_prop_c * tea_final_drywt_g,
+    tea_final_n_g = tea_prop_n * tea_final_drywt_g) %>%
+  # calculate the initial weight of nutrient per sample
+  mutate(
+    tea_initial_c_g = tea_prop_c * tea_initial_drywt_g,
+    tea_initial_n_g = tea_prop_n * tea_initial_drywt_g) %>%
+  # percent remaining
+  mutate(tea_pct_c_remain = tea_final_c_g / tea_initial_c_g * 100,
+         tea_pct_n_remain = tea_final_n_g / tea_initial_n_g * 100)
+
+# COMPARISONS ----
+# locations to look at 
+decomp_reduced.df %>%
+  ggplot(aes(sample_time, forage_pct_n_remain, color = crop)) +
+  stat_summary(fun = mean, geom = "point", na.rm = TRUE) +
+  stat_summary(fun.data = mean_se, geom = "errorbar", na.rm = TRUE) +
+  geom_point(na.rm = TRUE) +
+  facet_grid(location~crop)
+
+# bill to look at 
+bill.df %>%
+  ggplot(aes(sample_time, tea_pct_n_remain, color = crop)) +
+  stat_summary(fun = mean, geom = "point", na.rm = TRUE) +
+  stat_summary(fun.data = mean_se, geom = "errorbar", na.rm = TRUE) +
+  geom_point(na.rm = TRUE) +
+  facet_grid(location~crop)
 
 # now lets select out the columns we care about
 decomp_reduced.df <- decomp.df %>%
   select(location, crop, forage_id, tea_id, block, sample_time, forage_pct_remain, tea_pct_remain,
-         forage_pct_c_remaining, forage_pct_n_remaining, tea_pct_c_remaining, tea_pct_n_remaining)
+         forage_pct_c_remain, forage_pct_n_remain, tea_pct_c_remain, tea_pct_n_remain)
 
 # pivot longer
 reduced_long.df <- decomp_reduced.df %>%
@@ -134,6 +195,8 @@ reduced_long.df <- decomp_reduced.df %>%
                         tea_pct_c_remaining, tea_pct_n_remaining),
                names_to = "variable",
                values_to = "value")
+
+# WANT TO DO SOME PRELIMINARY PLOTTING TO FIND ANY WEIRD DATA ----
 
 # plotting it to see how things look
 reduced_long.df %>%
@@ -143,11 +206,31 @@ reduced_long.df %>%
   stat_summary(fun = mean, geom = "line", na.rm = TRUE) +
   facet_grid(location ~ variable)
 
+# locations to look at 
+decomp_reduced.df %>%
+  ggplot(aes(sample_time, tea_pct_n_remaining, color = crop)) +
+  stat_summary(fun = mean, geom = "point", na.rm = TRUE) +
+  stat_summary(fun.data = mean_se, geom = "errorbar", na.rm = TRUE) +
+  geom_point(na.rm = TRUE) +
+  facet_grid(location~crop)
+
+# PLACES I NEED TO CHECK THE DATA ----
+
+# forage percent remaining
+# ISU CR T6,7; ISU GPC T7; ISU PCRO T7; ISU WPC T5
+# WIU AR T5,6,9; WIU PCRO T7,9
+
+# forage carbon remaining 
+# ISU AR T1; ISU CR T2; ISU GPC T3; ISU WPC T2,5
+# WIU AR T9; WIU GPC T5; WIU WPC T6
+
+# SAVE OUTPUTS ----
+# percent remaining wide
+write_csv(decomp_reduced.df, file = "output/percent remaining wide.csv")
 
 
-
-
-
+# pct remaining long
+write_csv(reduced_long.df, file = "output/percent remaining long.csv")
 
 
 
